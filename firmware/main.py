3f1424641785d171b018c68d09e72b45c56bf73e
Amiya <developeramiya@gmail.com>
2025-07-08 21:32:35 -0700

    updated firmware

--- firmware/main.py
+++ firmware/main.py
@@ -1,4 +1,5 @@
 import board
+import busio
 
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
@@ -11,6 +12,27 @@ from kmk.modules.encoder import EncoderHandler
 from kmk.extensions.media_keys import MediaKeys
 
 keyboard = KMKKeyboard()
+
+keyboard.col_pins = (board.GP26, board.GP27, board.GP28)
+keyboard.row_pins = (board.GP3, board.GP4, board.GP2)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
+
+rgb = RGB(pixel_pin=board.GP29, num_pixels=9)
+
+# OLED, add stuff later
+
+driver = SSD1306(
+    i2c= busio.I2C(board.GP7, board.GP6)
+)
+
+keyboard.extensions.append(rgb)
+
+keyboard.keymap = [
+    [KC.N1, KC.N2, KC.N3], 
+    [KC.N4, KC.N5, KC.N6],
+    [KC.N7, KC.N8, KC.N9]
+    
+]
 
 if __name__ = '__main__':
     keyboard.go()
